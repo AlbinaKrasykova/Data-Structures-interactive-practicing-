f60a67b947969df633132cadf477cbf1263f32a5
Albina Krasykova <91033995+AlbinaKrasykova@users.noreply.github.com>
2025-09-07 08:35:10 -0400

    Update solution.py

--- my_solutions/solution.py
+++ my_solutions/solution.py
@@ -1,27 +1,13 @@
 #add your solution here
 
 def maxProfit(prices):
-    min_price = float('inf')
-    max_profit = 0
-    for price in prices:
-        if price < min_price:
-            min_price = price
-        elif price - min_price > max_profit:
-            max_profit = price - min_price
-    return max_profit
+  # TODO add you solution
 
 
 
 
 def twoSum(nums, target):
-    hashmap = {}
-    for i, num in enumerate(nums):
-        complement = target - num
-        if complement in hashmap:
-            return [hashmap[complement], i]
-        hashmap[num] = i
-    return []
-
+ # TODO add you solution
 
 
 
